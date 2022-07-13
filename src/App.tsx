412961b13cc9fa0970d4a2c9aa6d40d095590761
Andriusjok <andriusjokubauskass@gmail.com>
2022-07-13 12:45:12 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CssBaseline from '@mui/material/CssBaseline'
+import { Container, Grid, Typography } from '@mui/material'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './theme'
 
-function App() {
+export default function MyApp() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth={false}>
+        <Grid
+          container
+          justifyContent='center'
+          alignItems='center'
+          sx={{ height: '100vh', border: '1px solid #9eff49' }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Grid item container maxWidth='70vw'>
+            <Grid
+              item
+              xs={6}
+              sx={{
+                border: '1px solid grey',
+                height: 600,
+                backgroundColor: '#d6fff9',
+              }}
+            >
+              <Typography variant='h2'>SGV</Typography>
+            </Grid>
+            <Grid
+              item
+              xs={6}
+              sx={{
+                border: '1px solid grey',
+                height: 600,
+                backgroundColor: 'secondary.main',
+              }}
+            >
+              <Typography variant='h2'>CONTENT</Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
+    </ThemeProvider>
+  )
 }
-
-export default App;
